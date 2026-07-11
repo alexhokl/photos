1f8aa5d134cd034ccf6718996c721dc0ff4cd040
alexhokl <alexhokl@gmail.com>
2026-07-11 15:17:12 +0800

    Fixed kotlin build on Cloud Build

--- mobile/android/build.gradle.kts
+++ mobile/android/build.gradle.kts
@@ -19,6 +19,22 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+// Some plugins (e.g. photo_manager, cronet_http) don't pin a Kotlin jvmTarget,
+// which can drift from their Java target when the Kotlin Gradle Plugin version
+// changes its default. Force all subprojects to a consistent JVM 17 target to
+// avoid "Inconsistent JVM Target Compatibility Between Java and Kotlin Tasks".
+subprojects {
+    plugins.withId("org.jetbrains.kotlin.android") {
+        extensions.configure<org.jetbrains.kotlin.gradle.dsl.KotlinAndroidProjectExtension> {
+            compilerOptions.jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+        }
+    }
+    tasks.withType<JavaCompile>().configureEach {
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
